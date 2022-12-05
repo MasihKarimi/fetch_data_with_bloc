0a4b9126f14e263c1dd313134c68182d1a9f5a24
Masih Karimi <masihkarimi143@gmail.com>
2022-12-05 17:51:02 +0500

    User Model Has Been Created

--- lib/repos/repositories.dart
+++ lib/repos/repositories.dart
@@ -2,17 +2,22 @@ import 'dart:convert';
 
 import 'package:http/http.dart';
 
+import '../models/User.dart';
+
 class UserRepository {
   // from where we are going to fetch data
   String endPoint = "https://reqres.in/api/users?page=2";
 
   // this function is responsible for fetching data from the above endPoint with the help of http package
 
-  getUsers() async {
+  Future <List<UserModel>> getUsers() async {
     Response response = await get(Uri.parse(endPoint));
     // check response
     if (response.statusCode == 200) {
       final List result = jsonDecode(response.body)['data'];
+      // pass result to UserModel and convert to a List
+      return result.map((e) => UserModel.fromJson(e)).toList();
+
     } else {
       throw Exception(response.reasonPhrase);
     }
